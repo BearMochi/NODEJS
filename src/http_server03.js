186fd3bf6c8e0722e18dd418d9a155acf204e947
Daniel <a6511022000@gmail.com>
2021-03-09 14:02:47 +0800

    GOOD

---
+++ src/http_server03.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const fs = require('fs/promises');
+
+const server = http.createServer(async (req, res) => {
+    const str = await fs.readFile(__dirname + '/headers.json');
+    res.end(str);
+});
+server.listen(3000);

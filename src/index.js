2d55d0a76ed66ba344de1729f5b26fae6f2ac5cc
Daniel <a6511022000@gmail.com>
2021-03-10 10:51:36 +0800

    404

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
     res.send(`<h2>Hello</h2>`);
 });
 
+app.get('/123', (req, res) => {
+    res.send(`<h2>123</h2>`);
+});
+
+app.use((req, res) => {
+    res.status(404).send('<h1>找不到頁面404</h1>');
+});
+
 app.listen(process.env.PORT || 3000, () => {
     console.log('server started:', new Date());
 })

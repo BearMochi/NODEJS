44629498e10badfa61b4f40e28664b673ba1c85c
Daniel <a6511022000@gmail.com>
2021-03-10 11:26:51 +0800

    123

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 require('dotenv').config();
 const express = require('express');
+const fs = require('fs/promises');
 
 const app = express();
+
+app.use(express.static(__dirname + '/../public/404.html'));
 
 app.get('/', (req, res) => {
     res.send(`<h2>Hello</h2>`);
 });
 
-app.get('/123', (req, res) => {
-    res.send(`<h2>123</h2>`);
-});
-
-app.use((req, res) => {
-    res.status(404).send('<h1>找不到頁面404</h1>');
+app.use(async (req, res) => {
+    try {
+        data = await fs.readFile(__dirname + '/../public/404.html');
+        console.log(data);
+        res.status(404).send(data.toString());
+    } catch (ex) {
+        console.log(ex);
+        res.status(404).send('<h1>找不到頁面</h1>');
+    }
 });
 
 app.listen(process.env.PORT || 3000, () => {
